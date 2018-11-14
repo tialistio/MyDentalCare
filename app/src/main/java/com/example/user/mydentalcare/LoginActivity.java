883c272a7383079e95f09a079a36810f98002380
syntia <tia.listio@gmail.com>
2018-11-15 03:39:34 +0900

    Connected to Mysql Database (Login Page)

--- app/src/main/java/com/example/user/mydentalcare/LoginActivity.java
+++ app/src/main/java/com/example/user/mydentalcare/LoginActivity.java
@@ -1,12 +1,31 @@
 package com.example.user.mydentalcare;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText)findViewById(R.id.et_username);
+        password = (EditText)findViewById(R.id.et_password);
+    }
+
+    public void loginBtn(View view){
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
+        String type = "login";
+
+
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, user, pass);
     }
 }
