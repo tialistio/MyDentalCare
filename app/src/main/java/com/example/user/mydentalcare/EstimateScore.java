44a999d994f19e30168a326aaa85598523ddf4c0
Syntia Listio <tia.listio@gmail.com>
2018-11-29 03:55:48 +0900

    EstimateScore View Edited

--- app/src/main/java/com/example/user/mydentalcare/EstimateScore.java
+++ app/src/main/java/com/example/user/mydentalcare/EstimateScore.java
@@ -1,10 +1,13 @@
 package com.example.user.mydentalcare;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
 
 public class EstimateScore extends AppCompatActivity {
     @Override
@@ -13,6 +16,7 @@ public class EstimateScore extends AppCompatActivity {
         setContentView(R.layout.activity_estimatescore);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Estimate My Score");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -29,5 +33,33 @@ public class EstimateScore extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    //show the question detail
+
+    public void ShowDetail(View v){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(EstimateScore.this);
+        builder1.setCancelable(true);
+        AlertDialog alertDialog;
+
+        builder1.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        switch (v.getId()){
+            case R.id.tv_detail1:
+                builder1.setMessage("If all of your teeth are missing, you are no longer at risk for tooth decay or gum disease but you should still have dental check ups to observe your lips, cheeks, tongue, floor of the mouth and other soft tissues for signs of oral cancer or other sores that can develop at these sites.");
+                alertDialog = builder1.create();
+                alertDialog.show();
+                break;
+            case R.id.tv_detail2:
+                builder1.setMessage("A past history of fillings or crowns (caps) is often the best predictor for future needs. No dental restoration is permanent. The life expectancy of a filling or crown depends on many factors including diet, oral hygiene practices, and oral habits like bruxing, grinding, ice chewing or other possibly destructive oral habits.");
+                alertDialog = builder1.create();
+                alertDialog.show();
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/user/mydentalcare/Splash.java
+++ app/src/main/java/com/example/user/mydentalcare/Splash.java
@@ -31,7 +31,7 @@ public class Splash extends AppCompatActivity {
         Thread timer = new Thread(){
             public void run () {
                 try {
-                    sleep(5000);
+                    sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
