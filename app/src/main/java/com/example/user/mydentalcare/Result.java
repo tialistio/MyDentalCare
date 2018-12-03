b7879312de29bd6264fe93ece03c76b0cfc5b7b7
Syntia Listio <tia.listio@gmail.com>
2018-12-04 02:08:52 +0900

    Result activity Edited

--- app/src/main/java/com/example/user/mydentalcare/Result.java
+++ app/src/main/java/com/example/user/mydentalcare/Result.java
@@ -1,7 +1,10 @@
 package com.example.user.mydentalcare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.TextView;
 
 import static com.example.user.mydentalcare.EstimateScore2.totalscore2;
@@ -15,8 +18,30 @@ public class Result extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
+        //setting toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Estimate My Score");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //-----------------------------------------------
+
         score = (TextView)findViewById(R.id.tv_result_score);
         score.setText("Score : " + String.valueOf(totalscore2));
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        Intent intent = new Intent(Result.this, HomeActivity.class);
+        startActivity(intent);
+        return true;
     }
 }
